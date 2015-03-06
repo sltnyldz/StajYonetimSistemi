c7a3c149397846e3bdba349010f51e9ef38f2817
tubakaya <tuba_kaya@outlook.com>
2015-03-06 02:30:02 +0100

    Kullanici girisi refactoring
    - Gereksiz kodlar atildi
    - Kullanici girisi kutucugu _LoginPartial.cshtml'e alindi

--- StajYonetim/StajYonetim/Controllers/HomeController.cs
+++ StajYonetim/StajYonetim/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using StajYonetim.Models;
+using System.Linq;
 using System.Security.Claims;
 using System.Web.Mvc;
 
@@ -12,48 +13,12 @@ namespace StajYonetim.Controllers
             var userProfile = new UserProfile();
             if (Request.IsAuthenticated)
             {
-                var user = ClaimsPrincipal.Current.Identity;
-                userProfile.DisplayName = user.Name;
+                string displayName = ClaimsPrincipal.Current.Claims.First(c => c.Type.ToString().Contains("displayname")).Value;
+                userProfile.DisplayName = displayName;
             }
 
             return View(userProfile);
 
         }
-
-        // Asagidaki kodu ornek alarak kullanicinin hangi grupta oldugu bilgisine ulasicaz
-        // Bu bilgiye ulasmak icin GraphAPI denilen Azure Active Directory'nin APIsini cagirmamiz gerekecek...
-
-        //private const string TenantIdClaimType = "http://schemas.microsoft.com/identity/claims/tenantid";
-        //private const string LoginUrl = "https://login.windows.net/{0}";
-        //private const string GraphUrl = "https://graph.windows.net";
-        //private const string GraphUserUrl = "https://graph.windows.net/{0}/users/{1}?api-version=2013-04-05";
-        //private static readonly string AppPrincipalId = ConfigurationManager.AppSettings["ida:ClientID"];
-        //private static readonly string AppKey = ConfigurationManager.AppSettings["ida:Password"];
-
-        //[Authorize]
-        //public async Task<ActionResult> UserProfile()
-        //{
-        //    string tenantId = ClaimsPrincipal.Current.FindFirst(TenantIdClaimType).Value;
-
-        //    // Get a token for calling the Windows Azure Active Directory Graph
-        //    AuthenticationContext authContext = new AuthenticationContext(String.Format(CultureInfo.InvariantCulture, LoginUrl, tenantId));
-        //    ClientCredential credential = new ClientCredential(AppPrincipalId, AppKey);
-        //    AuthenticationResult assertionCredential = authContext.AcquireToken(GraphUrl, credential);
-        //    string authHeader = assertionCredential.CreateAuthorizationHeader();
-        //    string requestUrl = String.Format(
-        //        CultureInfo.InvariantCulture,
-        //        GraphUserUrl,
-        //        HttpUtility.UrlEncode(tenantId),
-        //        HttpUtility.UrlEncode(User.Identity.Name));
-
-        //    HttpClient client = new HttpClient();
-        //    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, requestUrl);
-        //    request.Headers.TryAddWithoutValidation("Authorization", authHeader);
-        //    HttpResponseMessage response = await client.SendAsync(request);
-        //    string responseString = await response.Content.ReadAsStringAsync();
-        //    UserProfile profile = JsonConvert.DeserializeObject<UserProfile>(responseString);
-
-        //    return View(profile);
-        //}
     }
 }
--- StajYonetim/StajYonetim/Models/UserProfile.cs
+++ StajYonetim/StajYonetim/Models/UserProfile.cs
@@ -4,7 +4,5 @@ namespace StajYonetim.Models
     public class UserProfile
     {
         public string DisplayName { get; set; }
-        public string GivenName { get; set; }
-        public string Surname { get; set; }
     }
 }
